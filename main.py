c1b3a96836df13ee2380b9035d96c7ff7515d6b8
Alperen Aga <alperencode@gmail.com>
2022-05-25 00:50:47 +0300

    Add basic files

--- main.py
+++
@@ -1,7 +0,0 @@
-from mido import MidiFile
-
-mid = MidiFile('VampireKillerCV1.mid', clip=True)
-
-for track in mid.tracks:
-    for msg in track:
-        print(msg)
---
+++ making_mashup.py
@@ -0,0 +1,19 @@
+import os
+
+from mido import MidiFile
+
+cv1 = MidiFile('new_song.mid', clip=True)
+cv3 = MidiFile('MIDI_sample.mid', clip=True)
+
+del cv1.tracks[4]
+del cv1.tracks[4]
+
+cv1.tracks.append(cv3.tracks[4])
+cv1.tracks.append(cv3.tracks[4])
+cv1.tracks.append(cv3.tracks[4])
+cv1.tracks.append(cv3.tracks[4])
+cv1.tracks.append(cv3.tracks[4])
+cv1.tracks.append(cv3.tracks[4])
+cv1.tracks.append(cv3.tracks[5])
+
+cv1.save('mashup.mid')
