ed93b4dfa9ca26462308b61f9180eb2f1ac5d189
Alperen Aga <alperencode@gmail.com>
2022-07-10 13:46:14 +0300

    Update converter

--- python/main_files/converter.py
+++ python/main_files/converter.py
@@ -1,8 +1,9 @@
-from music21 import * # Simple but not recommended
+from music21 import * 
 import os
-os.chdir('xmlsamples') # 'music' is my file location
 
-c = converter.parse('simple.mxl')
-bu = braille.translate.objectToBraille(c)
+os.chdir('xmlsamples') 
+
+converter = converter.parse('simple.mxl')
+bu = onverterbraille.translate.objectToBraille(c)
 ba = braille.basic.brailleUnicodeToBrailleAscii(bu)
 print(ba)
