94c0c869c4898e49db3ce183a8df754001addd97
Alperen Aga <alperencode@gmail.com>
2022-08-01 14:04:49 +0300

    Reset version

--- main_files/Midi-Project/Midi-Project.py
+++ main_files/Midi-Project/Midi-Project.py
@@ -449,7 +449,7 @@ def default_labels():
     app.config(menu=app_menu)
     about_menu = Menu(app_menu)
     app_menu.add_cascade(label="About", menu=about_menu)
-    about_menu.add_command(label="About", command=lambda: messagebox.showinfo("About", "This is an open-source software GUI program for sending and configuring Midi Signals.\n\nCreated by: Alperen Ağa\n\nVersion: 0.0.3"))
+    about_menu.add_command(label="About", command=lambda: messagebox.showinfo("About", "This is an open-source software GUI program for sending and configuring Midi Signals.\n\nCreated by: Alperen Ağa\n\nVersion: 0.0.1"))
     
     # Information menu
     information_menu = Menu(app_menu)
